5c2e199433b59d708c416ebb580ba19c27b0b8c8
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-02-03 11:20:36 +0100

    inversement rouge et bleu

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@ int main()
 {
     sil::Image image{"images/logo.png"};
     //Tout en vert
+//     for (glm::vec3& color : image.pixels())
+// {
+//     color.r = 0.f;
+//     color.b=0.f;
+// }
+
+
+//inversement couleur
     for (glm::vec3& color : image.pixels())
-{
-    color.r = 0.f;
-    color.b=0.f;
-}
+    std::swap(color.r, color.b);
+
+
     image.save("output/pouet.png");
 }
