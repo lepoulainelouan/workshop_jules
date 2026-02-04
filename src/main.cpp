4e1f73427b963d4d315dfe1fbdf3be4d6eb655b0
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-02-04 10:56:56 +0100

    rosace+cercle+disque+tgbsplit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <sil/sil.hpp>
 #include "random.hpp"
+#include <cmath>
+
 int main()
-{
-    sil::Image image{"images/logo.png"};
+// {
+//     sil::Image image{"images/logo.png"};
 
 
     //Tout en vert
@@ -75,13 +77,116 @@ int main()
 
 
 //rotation 90
+    // sil::Image new_image{image.width(), image.height()};
     // int total_pixels = image.width() * image.height();
     // for (int i = 0; i < total_pixels / 2; i++) {
     //     std::swap(image.pixels()[i], image.pixels()[total_pixels - 1 - i]);
     // }
-    // image.save("output/rotation_90.png");
+    // new_image.save("output/rotation_90.png");
     // }
 
 
 //RGB SPLIT
+//     sil::Image new_image{image.width(), image.height()};
+    
+//     int split =20;
+//     for (int y = 0; y < image.height(); y++) {
+//         for (int x = 0; x < image.width(); x++) {
+//             //Rouge
+//             int x_droite = std::min(x + split, image.width() - 1);
+//             new_image.pixel(x, y).r = image.pixel(x_droite, y).r;
+//             //Vert
+//             new_image.pixel(x, y).g = image.pixel(x, y).g;
+//             //Bleu
+//             int x_gauche = std::max(x - split, 0);
+//             new_image.pixel(x, y).b = image.pixel(x_gauche, y).b;
+//             }
+//     }
+//     new_image.save("output/logo_rgb_split.png");
+// }
 
+
+//disque 
+    // {
+    // sil::Image image{500/*width*/, 500/*height*/};
+    // int centre_x = 250;
+    // int centre_y = 250;
+    // int rayon = 100;
+    
+    // for (int y = 0; y < image.height(); y++) {
+    //     for (int x = 0; x < image.width(); x++) {
+    //         float distance = std::sqrt(
+    //             std::pow(x - centre_x, 2) + std::pow(y - centre_y, 2)
+    //         );
+    //         if (distance <= rayon) {
+    //             image.pixel(x, y) = glm::vec3{1.0f}; 
+    //         }
+    //         }
+    // } 
+    // image.save("output/disque.png");
+    // }
+
+
+//cercle
+    // {
+    // sil::Image image{500/*width*/, 500/*height*/};
+    // int centre_x = 250;
+    // int centre_y = 250;
+    // int rayon = 100;
+    // int thickness = 10;
+
+    // for (int y = 0; y < image.height(); y++) {
+    //     for (int x = 0; x < image.width(); x++) {
+    //         float distance = std::sqrt(
+    //             std::pow(x - centre_x, 2) + std::pow(y - centre_y, 2)
+    //         );
+    //         if (distance >= rayon - thickness && distance <= rayon) {
+    //             image.pixel(x, y) = glm::vec3{1.0f};
+    //         }
+    //         }
+    // }
+    // image.save("output/cercle.png");
+    // }
+
+
+//rosace
+    // {
+    // sil::Image image{500/*width*/, 500/*height*/};
+    // int centre_x = 250;
+    // int centre_y = 250;
+    // int rayon = 100;
+    // int thickness = 2;
+    
+    // for (int y = 0; y < image.height(); y++) {
+    //     for (int x = 0; x < image.width(); x++) {
+    //         bool dans_cercle = false;
+    //         float distance_centre = std::sqrt(
+    //             std::pow(x - centre_x, 2) + std::pow(y-centre_y, 2)
+    //         );
+    //         if (distance_centre >= rayon - thickness && distance_centre <= rayon) {
+    //             dans_cercle = true;
+    //         }
+
+    //         for (int i = 0; i < 6; i++) {//repetion du cercle 
+    //             float angle = i * 2.0f * M_PI / 6.0f;
+    //             int cx = centre_x + rayon * std::cos(angle);
+    //             int cy = centre_y + rayon * std::sin(angle);
+    //             float distance = std::sqrt(
+    //                 std::pow(x - cx, 2) + std::pow(y - cy, 2)
+    //             );
+                
+    //             if (distance >= rayon - thickness && distance <= rayon) {
+    //                 dans_cercle = true;
+    //             }
+    //         }
+            
+    //         if (dans_cercle) {
+    //             image.pixel(x, y) = glm::vec3{1.0f};
+    //         }
+    //     }
+    // }
+    // image.save("output/rosace.png");
+    // }
+
+//Mosaique
+    
